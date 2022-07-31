ad2cbc631210da9b00b2c4e633886027aea3ff94
yugo-s <y.saka0423@icloud.com>
2022-07-31 16:07:09 +0900

    Initial Commit

---
+++ TechPod/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  TechPod
+//
+//  Created by 坂口 友吾 on 2022/07/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDataSource {
+
+//    StoryBoardでTabViewを宣言
+    @IBOutlet var table:UITableView!
+     
+    var songNameArray = [String]()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        テーブルビューのデータソースメゾットはViewcontrollerクラスに書くよ、という設定
+        table.dataSource = self
+        
+        
+        songNameArray = ["カノン", "エリーゼのために", "G線上のアリア"]
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+//    セルの数を設定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+        
+    }
+    
+    
+//    ID付きのセルを取得して、セル付属のtextlabelに「テスト」と表示させてみる
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        cell?.textLabel?.text = "テスト"
+        
+        return cell!
+        
+    }
+
+    
+}
+
